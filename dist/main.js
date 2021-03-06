07e5f763ad6f8960a4ea887bd7efb8a38eab10e6
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2021-03-06 03:40:01 +0200

    Adding Plugin to webpack

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-alert("Hello FWD!");
+(()=>{"use strict";console.log((function(e){console.log("::: Running checkForName :::",e),["Picard","Janeway","Kirk","Archer","Georgiou"].includes(e)&&alert("Welcome, Captain!")})),alert("Hello FWD!")})();
